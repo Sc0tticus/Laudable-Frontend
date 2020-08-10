e2796e5ce4dc0224935f50853ae74c77a5d3d823
Scott Singer <ssinger303@gmail.com>
2020-08-09 22:11:57 -0600

    CH. 33 matches! Built out Registration form on the frontend

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,23 +1,67 @@
 import React from "react";
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import withStyles from "@material-ui/core/styles/withStyles";
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Gavel from "@material-ui/icons/Gavel";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 
 const Register = ({ classes }) => {
-  return <div >Register</div>;
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Gavel />
+        </Avatar>
+          <Typography variant="headline">
+            Register
+          </Typography>
+{/* 
+          <Mutation>
+            {() => {
+            
+              return ( } */}
+                <form className={classes.form}>
+                  <formControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="username">Username
+                    </InputLabel>
+                    <Input id="username" />
+                  </formControl>
+                  <formControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="email">Email
+                    </InputLabel>
+                    <Input id="email" type="email"/>
+                  </formControl>
+                  <formControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="password">Password
+                    </InputLabel>
+                    <Input id="password" type="password"/>
+                  </formControl>
+                  <Button type="submit" fullWidth variant="contained" color="secondary">
+                    Register
+                  </Button>
+                  <Button color="primary" variant="outlined" fullWidth>
+                    Previous user? Log in here
+                  </Button>
+                </form>
+              {/* );
+            }}
+          </Mutation> */}
+      </Paper>
+    </div>
+  );
 };
 
 const styles = theme => ({
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
-
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
+
+import Root from "./Root";
+import Auth from './components/Auth'
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql/'
@@ -13,7 +14,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
 <ApolloProvider client={client}>
-  <Root /> 
+  <Auth /> 
 </ApolloProvider>,
 document.getElementById("root"));
 
