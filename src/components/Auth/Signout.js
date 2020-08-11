c7122c68fb9147dcd45acebf79a677c70635b0e2
Scott Singer <ssinger303@gmail.com>
2020-08-11 14:39:44 -0600

    CH 44 good! Able to now signout users with the Signout Button

--- src/components/Auth/Signout.js
+++ src/components/Auth/Signout.js
@@ -1,21 +1,32 @@
 import React from "react";
+import { ApolloConsumer } from 'react-apollo'
 import withStyles from "@material-ui/core/styles/withStyles";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
 const Signout = ({ classes }) => {
+  const handleSignout = client => {
+    localStorage.removeItem('authToken')
+    client.writeData({ data: { isLoggedIn: false  }});
+    console.log('Signed out user', client );
+  };
+
   return (
-    <Button>
-      <Typography
-        variant="body1"
-        className={classes.buttonText}
-        color='secondary'
+    <ApolloConsumer>
+    {client => (
+      <Button onClick={() => handleSignout(client)}>
+        <Typography
+          variant="body1"
+          className={classes.buttonText}
+          color='secondary'
       >
       Signout
-      </Typography>
-      <ExitToApp className={classes.buttonIcon} color="secondary" />
-    </Button>
+        </Typography>
+        <ExitToApp className={classes.buttonIcon} color="secondary" />
+      </Button>
+    )}
+    </ApolloConsumer>
   );
 };
 
