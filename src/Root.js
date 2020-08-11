2a3734eee95a5da4e759f46c7fb7ef47f87d53b3
Scott Singer <ssinger303@gmail.com>
2020-08-11 13:36:50 -0600

    CH 42 good! Added Routing with React Router to Root.js

--- src/Root.js
+++ src/Root.js
@@ -1,8 +1,14 @@
 import React from "react";
-import withRoot from "./withRoot";
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Query } from 'react-apollo'
 import {gql} from 'apollo-boost'
+
+import withRoot from "./withRoot";
+import App from './pages/App'
+import Profile from './pages/Profile';
+import Header from './components/Shared/Header'
+
+
 
 const Root = () => (
   <Query query={ME_QUERY}>
@@ -10,7 +16,17 @@ const Root = () => (
       if (loading) return <div>Loading</div>
       if (error) return <div>Error</div>
 
-      return <div>{JSON.stringify(data)}</div>
+      return (
+        <Router>
+          <>
+          <Header />
+          <Switch>
+            <Route exact path = "/" component={App} />
+            <Route path="/profile/:id" component={Profile}/>
+          </Switch>
+          </>
+        </Router>
+      )
     }}
   </Query>
 );
