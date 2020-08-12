4b609db81f7233775679db050fde0ac57828b153
Scott Singer <ssinger303@gmail.com>
2020-08-11 20:39:11 -0600

    CH 46 success! Able to display a Users tracks to the  web page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import withStyles from "@material-ui/core/styles/withStyles";
 
+import SearchTracks from '../components/Track/SearchTracks';
+import TrackList from '../components/Track/TrackList';
+import CreateTrack from '../components/Track/CreateTrack';
+import Loading from '../components/Shared/Loading';
+import Error from '../components/Shared/Error'
+
+
+
 const App = ({ classes }) => {
-  return <div>App</div>;
+  return (
+    <div className={classes.container}>
+      <SearchTracks />
+      <CreateTrack />
+      <Query query={GET_TRACKS_QUERY}>
+        {({data, loading, error }) => {
+          if (loading) return <Loading />
+          if (error) return <Error error={error} />
+
+          return <TrackList tracks={data.tracks} />
+        }}
+      </Query>
+    </div>
+  );
 };
+
+const GET_TRACKS_QUERY = gql`
+  query getTracksQuery {
+    tracks {
+      id 
+      title 
+      description 
+      url 
+      likes {
+        id
+      }
+      postedBy {
+        id 
+        username
+      }
+    }
+  }
+`
+
+
 
 const styles = theme => ({
   container: {
