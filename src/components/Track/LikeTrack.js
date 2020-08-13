8973b762ff8bdddb67e56a08c2727c93f0369df2
Scott Singer <ssinger303@gmail.com>
2020-08-13 15:15:18 -0600

    CH 59 6S! Created LIKES/LIKE buttoon for Tracks

--- src/components/Track/LikeTrack.js
+++ src/components/Track/LikeTrack.js
@@ -1,11 +1,47 @@
 import React from "react";
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import withStyles from "@material-ui/core/styles/withStyles";
-// import IconButton from "@material-ui/core/IconButton";
-// import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import IconButton from "@material-ui/core/IconButton";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
-const LikeTrack = ({ classes }) => {
-  return <div >LikeTrack</div>;
+const LikeTrack = ({ classes, trackId, likeCount }) => {
+  return (
+    <Mutation 
+      mutation={CREATE_LIKE_MUTATION}
+      variables={{ trackId }}
+      onCompleted={data => {
+        console.log({ data })
+      }}
+    >
+    {createLike => (
+      <IconButton onClick={event => {
+        event.stopPropagation();
+        createLike()
+      }}
+        className={classes.iconButton}
+      >
+      {likeCount}
+      <ThumbUpIcon className={classes.icon} />
+      </IconButton>
+    )}
+    </Mutation>
+  );
 };
+
+const CREATE_LIKE_MUTATION = gql`
+  mutation($trackId: Int!){
+    createLike(trackId: $trackId){
+      track {
+        id 
+        likes {
+          id
+        }
+      }
+    }
+  }
+`
+
 
 const styles = theme => ({
   iconButton: {
