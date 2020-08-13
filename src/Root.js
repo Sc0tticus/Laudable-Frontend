bf4263e6b4bd739e8acbf7db6b2c811f4c755936
Scott Singer <ssinger303@gmail.com>
2020-08-13 15:35:49 -0600

    CH 59 6S! Disabled mutiple LIKES per Track

--- src/Root.js
+++ src/Root.js
@@ -35,12 +35,17 @@ const Root = () => (
   </Query>
 );
 
-const ME_QUERY = gql`
+export const ME_QUERY = gql`
 {
   me {
     id
     username
     email
+    likeSet {
+      track {
+        id
+      }
+    }
   }
 }
 `
--- src/components/Track/LikeTrack.js
+++ src/components/Track/LikeTrack.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import withStyles from "@material-ui/core/styles/withStyles";
 import IconButton from "@material-ui/core/IconButton";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
+import { UserContext, ME_QUERY } from '../../Root'
+
 const LikeTrack = ({ classes, trackId, likeCount }) => {
+  const currentUser = useContext(UserContext)
+
+  const handleDisableLikedTrack = () => {
+    const userLikes = currentUser.likeSet
+    const isTrackLiked = 
+    userLikes.findIndex(({ track }) => track.id === trackId) > -1;
+    return isTrackLiked;
+  }
+
+
   return (
     <Mutation 
       mutation={CREATE_LIKE_MUTATION}
       variables={{ trackId }}
       onCompleted={data => {
-        console.log({ data })
+        console.log({ data });
       }}
+      refetchQueries={() => [{ query: ME_QUERY }]}
     >
     {createLike => (
-      <IconButton onClick={event => {
+      <IconButton 
+        onClick={event => {
         event.stopPropagation();
-        createLike()
+        createLike();
       }}
         className={classes.iconButton}
+        disabled={handleDisableLikedTrack()}
       >
       {likeCount}
       <ThumbUpIcon className={classes.icon} />
