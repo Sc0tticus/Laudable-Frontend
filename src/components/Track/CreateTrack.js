74deef53c29883a72596d040cefebb1ed1edcd09
Scott Singer <ssinger303@gmail.com>
2020-08-12 19:09:02 -0600

    CH 50 match! Ability to upload tracks and have   them added to the page instantly

--- src/components/Track/CreateTrack.js
+++ src/components/Track/CreateTrack.js
@@ -17,6 +17,7 @@ import AddIcon from "@material-ui/icons/Add";
 import ClearIcon from "@material-ui/icons/Clear";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
+import { GET_TRACKS_QUERY } from '../../pages/App'
 import Error from '../Shared/Error'
 
 const CreateTrack = ({ classes }) => {
@@ -71,6 +72,7 @@ const CreateTrack = ({ classes }) => {
         setSubmitting(false);
         setOpen(false);
       }}
+      refetchQueries={() => [{ query: GET_TRACKS_QUERY}]}
       >
       {(createTrack, { loading, error}) => {
         if (error) return <Error error={error} />
--- src/pages/App.js
+++ src/pages/App.js
@@ -28,7 +28,7 @@ const App = ({ classes }) => {
   );
 };
 
-const GET_TRACKS_QUERY = gql`
+export const GET_TRACKS_QUERY = gql`
   query getTracksQuery {
     tracks {
       id 
